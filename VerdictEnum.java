665642314dc9fcc293838a1ff5e1bd526f269518
Tashinga <tashinga@tashingas-air.home>
2019-11-29 12:14:33 +0000

    update

---
+++ VerdictEnum.java
@@ -0,0 +1,12 @@
+public class VerdictEnum{
+
+    enum Verdict{
+        Champion, WeakAccept, WeakReject, Detractor
+    }
+
+    public static void main (String[] args){
+        Verdict lol = Verdict.Champion; 
+        //System.out.println(lol); 
+    }
+
+}
