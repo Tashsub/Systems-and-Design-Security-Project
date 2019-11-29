de1bef8613b7162d3dcbf6fab48cb5aa9b1e6673
Tashinga <tashinga@tashingas-air.home>
2019-11-29 12:08:24 +0000

    new class

---
+++ Roles.java
@@ -0,0 +1,19 @@
+public class Roles{
+
+    User user; 
+
+    public Roles(User user){
+        this.user = user; 
+    }
+
+    public void passChiefEditor(User user){
+    }
+        
+    public void chooseChiefEditor(decide){
+    //has to select another editor from the journal 
+    }
+
+    public void passmainAuthor(User user){
+
+    }
+}
